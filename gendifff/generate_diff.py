9e6d347b359bb68b080040e19c5b9b793ba20963
Maksim Lesnikov <maximborzya75@gmail.com>
2023-01-23 16:29:08 +0700

    update func generate_diff, create Makefile and add make lint, add setup.cfg

--- gendifff/generate_diff.py
+++ gendifff/generate_diff.py
@@ -1,33 +1,39 @@
 import json
 
 
+def get_format(value):
+    if value is True:
+        return 'true'
+    elif value is False:
+        return 'false'
+    elif value is None:
+        return 'null'
+    return value
+
+
 def generate_diff(file_path1, file_path2) -> str:
-    file1 = dict(sorted(json.load(open(str(file_path1))).items(), key=lambda x: x[0]))
-    file2 = dict(sorted(json.load(open(str(file_path2))).items(), key=lambda x: x[0]))
+    file1 = json.load(open(file_path1))
+    file2 = json.load(open(file_path2))
+    file1_sorted = dict(sorted(file1.items(), key=lambda x: x[0]))
+    file2_sorted = dict(sorted(file2.items(), key=lambda x: x[0]))
 
     result = ''
-    for key1 in file1:
-        value1 = file1.get(key1, None)
-        if isinstance(value1, bool):
-            value1 = str(value1).lower()
+    for key1 in file1_sorted:
+        value1 = get_format(file1_sorted.get(key1, None))
+        value2 = get_format(file2_sorted.get(key1, None))
 
-        if key1 not in file2:
+        if key1 not in file2_sorted:
             result += f' - {key1}: {value1}\n'
         else:
-            value2 = file2.get(key1, None)
             if value1 == value2:
                 result += f'   {key1}: {value1}\n'
-                file2.pop(key1)
             else:
                 result += f' - {key1}: {value1}\n'
                 result += f' + {key1}: {value2}\n'
-                file2.pop(key1)
+            file2_sorted.pop(key1)
 
-    for key2 in file2:
-        value2 = file2.get(key2, None)
-        if isinstance(value2, bool):
-            value2 = str(value2).lower()
-
+    for key2 in file2_sorted:
+        value2 = get_format(file2_sorted.get(key2, None))
         result += f' + {key2}: {value2}\n'
 
     return '{\n' + result + '}'
