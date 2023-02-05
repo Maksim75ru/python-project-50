de515d903d660b3e2ca821c1a4bb14b5c5e61d99
Maksim Lesnikov <maximborzya75@gmail.com>
2023-02-05 23:50:07 +0700

    fix some lint bags

--- gendifff/formatters/plain_format.py
+++ gendifff/formatters/plain_format.py
@@ -9,10 +9,10 @@ def get_complex_value(val):
     return f"'{val}'"
 
 
-def get_stylish(diff: dict, keys=None) -> str:
+def get_stylish(diff: dict, keys=None) -> str:  # noqa: C901
     lines = []
     parents = ''
-    status_key = {
+    output_msg = {
         'changed': "Property '{path}' was updated. From {old} to {new}",
         'removed': "Property '{path}' was removed",
         'added': "Property '{path}' was added with value: {val}"
@@ -30,12 +30,17 @@ def get_stylish(diff: dict, keys=None) -> str:
             if status == 'changed':
                 old_val = get_complex_value(val.get('old_value'))
                 new_val = get_complex_value(val.get('new_value'))
-                lines.append(f"{status_key[status].format(path=parents + key, old=old_val, new=new_val)}")
+                lines.append(output_msg[status].format(
+                    path=parents + key,
+                    old=old_val,
+                    new=new_val))
             if status == 'removed':
-                lines.append(f"{status_key[status].format(path=parents + key)}")
+                lines.append(output_msg[status].format(path=parents + key))
             if status == 'added':
                 work_value = get_complex_value(val.get('value'))
-                lines.append(f"{status_key[status].format(path=parents + key, val=work_value)}")
+                lines.append(output_msg[status].format(
+                    path=parents + key,
+                    val=work_value))
 
     return '\n'.join(lines)
 
