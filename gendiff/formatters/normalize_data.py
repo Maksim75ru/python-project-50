4655320cf2289109c298d016d1728cdc052c057a
Maksim Lesnikov <maximborzya75@gmail.com>
2023-02-06 22:53:15 +0700

    fix some tests

--- gendiff/formatters/normalize_data.py
+++ gendiff/formatters/normalize_data.py
@@ -11,7 +11,8 @@ def normalize_values(data: dict) -> dict:
         for k, v in data.items():
             if isinstance(v, dict):
                 normalize_values(v)
-            elif isinstance(v, (bool, type(None))) or \
-                    v in correct_values.keys():
+            elif isinstance(v, (bool, type(None))):
+            # elif isinstance(v, (bool, type(None))) or \
+            #         v in correct_values.keys():
                 data[k] = correct_values[v]
     return data
