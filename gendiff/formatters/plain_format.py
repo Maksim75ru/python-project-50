cdca9fe79d2503a3c70d7e09ce3a1e249eb14704
Maksim Lesnikov <maximborzya75@gmail.com>
2023-02-10 13:50:48 +0700

    fix plain_format.py remove a condition to deacrese cognitive complexity

--- gendiff/formatters/plain_format.py
+++ gendiff/formatters/plain_format.py
@@ -26,21 +26,20 @@ def get_plain(diff: dict, keys=None) -> str:  # noqa: C901
 
         if status == 'nested':
             lines.append(f"{get_plain(value.get('value'), parents + key)}")
-        else:
-            if status == 'changed':
-                old_val = get_complex_value(value.get('old_value'))
-                new_val = get_complex_value(value.get('new_value'))
-                lines.append(output_msg[status].format(
-                    path=parents + key,
-                    old=old_val,
-                    new=new_val))
-            if status == 'removed':
-                lines.append(output_msg[status].format(path=parents + key))
-            if status == 'added':
-                work_value = get_complex_value(value.get('value'))
-                lines.append(output_msg[status].format(
-                    path=parents + key,
-                    val=work_value))
+        if status == 'changed':
+            old_val = get_complex_value(value.get('old_value'))
+            new_val = get_complex_value(value.get('new_value'))
+            lines.append(output_msg[status].format(
+                path=parents + key,
+                old=old_val,
+                new=new_val))
+        if status == 'removed':
+            lines.append(output_msg[status].format(path=parents + key))
+        if status == 'added':
+            work_value = get_complex_value(value.get('value'))
+            lines.append(output_msg[status].format(
+                path=parents + key,
+                val=work_value))
 
     return '\n'.join(lines)
 
