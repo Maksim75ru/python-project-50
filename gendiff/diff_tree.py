435c98c53576976de96d95d3ba764a78cd3744d2
Maksim Lesnikov <maximborzya75@gmail.com>
2023-02-10 14:10:49 +0700

    reverted the changes

--- gendiff/diff_tree.py
+++ gendiff/diff_tree.py
@@ -18,10 +18,11 @@ def get_diff(tree1: dict, tree2: dict) -> dict:  # noqa: C901
                 {'status': 'nested', 'value': get_diff(old_val, new_val)}
         elif old_val == new_val:
             result[key] = {'status': 'unchanged', 'value': old_val}
-        elif old_val != new_val and old_val == 'not_exists':
-            result[key] = {'status': 'added', 'value': new_val}
-        else:
-            result[key] = {'status': 'changed',
-                           'old_value': old_val, 'new_value': new_val}
+        elif old_val != new_val:
+            if old_val == 'not_exists':
+                result[key] = {'status': 'added', 'value': new_val}
+            else:
+                result[key] = {'status': 'changed',
+                               'old_value': old_val, 'new_value': new_val}
 
     return dict(sorted(result.items()))
